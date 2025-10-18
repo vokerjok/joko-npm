ab849b892856a2e4f4677d84abb5e2df86a500ea
Voker <80169267+vokerjok@users.noreply.github.com>
2025-10-18 15:08:52 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "index.js",
+  input: "src/index.js",
   output: {
     file: "dist/joko.mjs",
     format: "esm",
